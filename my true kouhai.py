9bcd957c55534de30cdb70101b57c44e901c46b9
InterChan <interchanmc@gmail.com>
2022-04-24 23:28:36 +0800

    toggle know'ers

--- my true kouhai.py
+++ my true kouhai.py
@@ -1,12 +1,18 @@
 import discord
 import logging
+import json
+from os.path import exists
 
 logging.basicConfig(level=logging.INFO)
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
+
+knowers_blacklist = []
+if exists("knowers_blacklist.json"):
+    with open("knowers_blacklist.json", "r") as knowers_blacklist_file:
+        knowers_blacklist = json.load(knowers_blacklist_file)
 
 @client.event
 async def on_ready():
@@ -17,9 +23,30 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    words = message.content.split()
-    for word in words:
-        if word.endswith("er"):
-            await message.channel.send(f"{word.capitalize()}? I hardly know 'er!")
+    if message.content == "toggle know'ers":
+        if message.channel.id in knowers_blacklist:
+            # remove from blacklist to enable
+            knowers_blacklist.remove(message.channel.id)
+            await message.channel.send(f"enabled know'ers here (channel id {message.channel.id})")
+        else:
+            # add to blacklist to disable
+            knowers_blacklist.append(message.channel.id)
+            await message.channel.send(f"disabled know'ers here (channel id {message.channel.id})")
+        with open("knowers_blacklist.json", "w") as knowers_blacklist_file:
+            json.dump(knowers_blacklist, knowers_blacklist_file)
 
-client.run(open("token.xd", "r").read())
+
+    if message.channel.id not in knowers_blacklist:
+        words = message.content.split()
+        for word in words:
+            if "er" in word.lower():
+                # ardly = ""
+                # for char in "I hardly know her":
+                #     if random() < 2/17:
+                #         ardly += "'"
+                #     else:
+                #         ardly += char
+                await message.channel.send(f"{word.capitalize()}? I 'ardly know 'er{word.lower().split('er')[-1]}!")
+
+with open("token.xd", "r") as xd:
+    client.run(xd.read())
