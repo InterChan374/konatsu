fa685e91cdd480cde956620f717010be7db5b683
InterChan <interchanmc@gmail.com>
2022-04-24 22:01:24 +0800

    i hardly knower
    peak coding right here

--- my true kouhai.py
+++ my true kouhai.py
@@ -1,4 +1,7 @@
 import discord
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,7 +17,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    words = message.content.split()
+    for word in words:
+        if word.endswith("er"):
+            await message.channel.send(f"{word.capitalize()}? I hardly know 'er!")
 
-client.run(open("token.txt", "r").read())
+client.run(open("token.xd", "r").read())
